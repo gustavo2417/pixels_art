1d3593e15b8e1b630291d139d0d82dc4ee0e93e7
Gustavo <gustavodarocha241@gmail.com>
2022-05-04 04:38:39 -0300

    requisito 9 completo

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 let origin = document.getElementById("pixel-board");
 let clicks = document.getElementsByClassName("color")
 
-
-
 function blocks() {
   for (let index = 0;index < 5;index += 1){
     let coluna = document.createElement("div")
-    coluna.className = "line-ll"
+    coluna.classList.add("line-ll")
 
     for (let ind = 0;ind < 5;ind += 1) {
       let PixelsLenght = document.createElement("div")
-      PixelsLenght.className = "pixel"
+      PixelsLenght.classList.add("pixel")
       coluna.appendChild(PixelsLenght)
      }
     origin.appendChild(coluna)
@@ -18,7 +16,7 @@ function blocks() {
 }
 blocks()
 
-function selector () {
+function selector() {
   for(let i = 0; i < clicks.length; i += 1) {
     clicks[i].addEventListener("click",newSelector)
   }
@@ -32,7 +30,7 @@ function newSelector(click) {
   click.target = (click.target).classList.add("selected")
 } 
 
-function paint () {
+function paint() {
   let square = document.querySelectorAll(".pixel")
   for(let b = 0; b < square.length; b += 1) {
     square[b].addEventListener("click", attach)
@@ -40,10 +38,17 @@ function paint () {
 }
 paint ()
 
-function attach (whichColor) {
+function attach(whichColor) {
   let selecionar = document.querySelector(".selected")
   let newcolor = window.getComputedStyle(selecionar).backgroundColor
   let before = (whichColor.target).style.backgroundColor = newcolor
   before = newcolor
+}
+
+function clean() {
+  let get = document.querySelectorAll(".pixel")
+  for (let c = 0; c < get.length;c += 1) {
+    get[c] = get[c].style.backgroundColor = "white"
+  }
 }
 
